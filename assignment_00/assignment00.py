6e1426861e77308330e47c29f87bd3405a5d8cd8
Berenice Medel <bemesa21@gmail.com>
2020-02-09 15:00:12 -0600

    changes number conv

--- assignment_00/assignment00.py
+++ assignment_00/assignment00.py
@@ -3,8 +3,14 @@ import fileinput
 def read_input():
     lines = []
     for line in fileinput.input():
-        lines.append(float(line.strip()))
+        lines.append(convert_num(line.strip()))
     return lines
+
+def convert_num(num):
+    try:
+        return int(num)
+    except ValueError:
+        return float(num)
 
 values = read_input()
 print(sum(values))
